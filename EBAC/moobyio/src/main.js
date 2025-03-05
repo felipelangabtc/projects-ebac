d0c3a435679f4b4b77f4e91efa47f6ee0495e284
Felipe Langa <felipelanga95@gmail.com>
2025-03-05 01:28:17 -0300

    Atualização

--- EBAC/moobyio/src/main.js
+++ EBAC/moobyio/src/main.js
@@ -1,6 +1,22 @@
 document.addEventListener('DOMContentLoaded', function() {
     const buttons = document.querySelectorAll('[data-tab-button]');
-    
+    const questions = document.querySelectorAll('[data-faq-question');
+
+    const heroSection = document.querySelector('.hero');
+    const heightHero = heroSection.clientHeight;
+
+    window.addEventListener('scroll', function() {
+        const currentPosition = window.scrollY;
+
+        if (currentPosition < heightHero) {
+            hiddenHeaderElements();
+        } else {
+            showHeaderElements();
+        }
+    })
+
+
+    // Tabs "Most Popular", "Quests/events", "More Islands"
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
@@ -12,7 +28,29 @@ document.addEventListener('DOMContentLoaded', function() {
         })
     }
     
+    // F.A.Q.
+    for (let i = 0; i < questions.length; i++) {
+        questions[i].addEventListener('click', openOrCloseAnswer);
+    }
+
 })
+
+function hiddenHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.add('header--is-hidden');
+}
+
+function showHeaderElements() {
+    const header = document.querySelector('header');
+    header.classList.remove('header--is-hidden');
+}
+
+function openOrCloseAnswer(elemento) {
+    const classe = 'faq__questions__item--is-open';
+    const elementoPai = elemento.target.parentNode;
+
+    elementoPai.classList.toggle(classe);
+}
 
 function removeBotaoAtivo() {
     const buttons = document.querySelectorAll('[data-tab-button]');
